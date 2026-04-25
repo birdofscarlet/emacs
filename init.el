1b8e09b3ed14314bf477a3f9a8f7e7fbb56c2be2
Scarlet <cardinalsnest@protonmail.com>
2026-04-25 11:52:28 -0500

    i forgot

--- init.el
+++ init.el
@@ -45,6 +45,69 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
+
+; ---------------------------------- ;
+;;             KEYBINDS             ;;
+; ---------------------------------- ;
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
 ; ---------------------------------- ;
 ;;        PACKAGE MANAGEMENT        ;;
 ; ---------------------------------- ;
+
+;; trying not to use too many packages but some stuff
+;; i really would rather have and base emacs doesn't have it
+
+(require 'package)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("org"   . "https://orgmode.org/elpa/")
+        ("elpa"  . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; for non linux
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; discord
+(use-package elcord)
+(require 'elcord)
+(elcord-mode)
+(setq elcord-quiet t)
+
+;; company
+(use-package company)
+(require 'company)
+(company-mode)
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+
+;; powershell
+(use-package powershell)
+(require 'powershell)
+
+;; TODO: can we get rid of this somehow
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elcord-editor-icon "emacs_pen_icon")
+ '(elcord-idle-message "editing *real-life*")
+ '(package-selected-packages '(company company-box elcord lsp-pwsh powershell)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
